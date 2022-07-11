d3409b5dc04d8dec8a5b064f5f84d415e8417c71
divyanshbhargava <109080286+divyanshbhargava@users.noreply.github.com>
2022-07-11 20:08:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Divyansh Bhargava";
+document.getElementById("roll-no").innerHTML="201500231";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString();
